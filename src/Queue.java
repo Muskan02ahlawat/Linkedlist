3990fbbee6e1be1b5406b730cb80ec13930b13cb
Muskan02ahlawat <98382491+Muskan02ahlawat@users.noreply.github.com>
2023-09-18 11:32:15 +0530

    Update Queue.java

--- src/Queue.java
+++ src/Queue.java
@@ -10,22 +10,17 @@ public class Queue {
         }
         static void queueEnqueue(int data)
         {
-            // check queue is full or not
+        
             if (capacity == rear) {
                 System.out.printf("\nQueue is full\n");
                 return;
             }
-
-            // insert element at the rear
             else {
                 queue[rear] = data;
                 rear++;
             }
             return;
         }
-
-        // function to delete an element
-        // from the front of the queue
         static void queueDequeue()
         {
             // if queue is empty
@@ -40,18 +35,12 @@ public class Queue {
                 for (int i = 0; i < rear - 1; i++) {
                     queue[i] = queue[i + 1];
                 }
-
-                // store 0 at rear indicating there's no element
                 if (rear < capacity)
                     queue[rear] = 0;
-
-                // decrement rear
                 rear--;
             }
             return;
         }
-
-        // print queue elements
         static void queueDisplay()
         {
             int i;
@@ -59,15 +48,12 @@ public class Queue {
                 System.out.printf("\nQueue is Empty\n");
                 return;
             }
-
-            // traverse front to rear and print elements
             for (i = front; i < rear; i++) {
                 System.out.printf(" %d <-- ", queue[i]);
             }
             return;
         }
 
-        // print front of queue
         static void queueFront()
         {
             if (front == rear) {
@@ -81,29 +67,17 @@ public class Queue {
     }
 
     class StaticQueueinjava {
-
-        // Driver code
         public static void main(String[] args)
         {
-            // Create a queue of capacity 4
             Queue q = new Queue(4);
-
-            // print Queue elements
             q.queueDisplay();
-
-            // inserting elements in the queue
             q.queueEnqueue(20);
             q.queueEnqueue(30);
             q.queueEnqueue(40);
             q.queueEnqueue(50);
-
-            // print Queue elements
-            q.queueDisplay();
-
-            // insert element in the queue
+            q.queueDisplay()
             q.queueEnqueue(60);
 
-            // print Queue elements
             q.queueDisplay();
 
             q.queueDequeue();
@@ -111,10 +85,7 @@ public class Queue {
             System.out.printf(
                     "\n\nafter two node deletion\n\n");
 
-            // print Queue elements
             q.queueDisplay();
-
-            // print front of the queue
             q.queueFront();
         }
     }
