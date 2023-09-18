b6ead14fdc4ec4f99b11f8fb8e03629179f4c251
Muskan02ahlawat <98382491+Muskan02ahlawat@users.noreply.github.com>
2023-09-18 11:29:48 +0530

    Update Deletion.java

--- src/Deletion.java
+++ src/Deletion.java
@@ -18,36 +18,22 @@ public class Deletion {
             }
             return count;
         }
-
         static Node deleteMid(Node head)
         {
-            // Base cases
             if (head == null)
                 return null;
             if (head.next == null) {
                 return null;
             }
             Node copyHead = head;
-
-            // Find the count of nodes
             int count = countOfNodes(head);
-
-            // Find the middle node
             int mid = count / 2;
-
-            // Delete the middle node
             while (mid-- > 1) {
                 head = head.next;
             }
-
-            // Delete the middle node
             head.next = head.next.next;
-
             return copyHead;
         }
-
-        // A utility function to print
-        // a given linked list
         static void printList(Node ptr)
         {
             while (ptr != null) {
@@ -56,21 +42,15 @@ public class Deletion {
             }
             System.out.println("NULL");
         }
-
-        /* Driver code*/
         public static void main(String[] args)
         {
-            /* Start with the empty list */
             Node head = newNode(1);
             head.next = newNode(2);
             head.next.next = newNode(3);
             head.next.next.next = newNode(4);
-
             System.out.println("Given Linked List");
             printList(head);
-
             head = deleteMid(head);
-
             System.out.println(
                     "Linked List after deletion of middle");
             printList(head);
